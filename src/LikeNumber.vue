8c732d1fde3c827a4907b12b81afe37e4bafa8f9
ash <satoshi.0411@icloud.com>
2021-02-07 20:14:44 +0900

    コンポーネント 一つの要素でくくる

---
+++ src/LikeNumber.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>いいね{{ number }}</p>
+    <button @click="increment">プラスされるやで</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      number: 100
+    }
+  },
+  methods: {
+    increment: function() {
+      this.number += 10
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App.vue' //
+import LikeNumber from './LikeNumber.vue'
 
 Vue.config.productionTip = false
+Vue.component('LikeNumber', LikeNumber)
 
+// Appはコンポーネントのオブジェクト
 new Vue({
-  render: h => h(App),
+  render: h => h(App), //ES6の書き方
 }).$mount('#app')
+
+
+
+// render: h => ('div', 'こんにちは')
+// render: function (h) {
+//   return h(App)
+// }
