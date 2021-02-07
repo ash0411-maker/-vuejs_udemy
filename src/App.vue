819a6a8fe8ea2319b150c359d1b5cffe4afc1d21
ash <satoshi.0411@icloud.com>
2021-02-07 20:19:30 +0900

    ローカル登録

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
 <template>
-  <LikeNumber></LikeNumber>
+  <div>
+    <LikeHeader></LikeHeader>
+    <LikeNumber></LikeNumber>
+  </div>
 </template>
+
+<script>
+import LikeHeader from "./LikeHeader"
+
+export default {
+  components: {
+    // LikeHeader: LikeHeader ES5
+    LikeHeader //ES6
+  }
+}
+</script>
---
+++ src/LikeHeader.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>いいねのためのいいねページ</h1>
+</template>
