cbb41bb5caf4c571a74ba128ff08b29f0fc17c9c
ash <satoshi.0411@icloud.com>
2021-02-14 20:59:55 +0900

    input radio v-model

--- src/components/App.vue
+++ src/components/App.vue
@@ -45,6 +45,13 @@
       <input type="checkbox" id="30" value="30" v-model="eventData.target">
       <label for="10">30代</label>
       <p> {{ eventData.target }}</p>
+
+      <p>参加費</p>
+      <input type="radio" id="free" value="無料" v-model="eventData.price">
+      <label for="free">無料</label>
+      <input type="radio" id="paid" value="有料" v-model="eventData.price">
+      <label for="paid">有料</label>
+      <p>{{ eventData.price }}</p>
     </div>
   </div>
 </template>
@@ -62,6 +69,7 @@ export default {
         detail: "",
         isPrivate: false,
         target: [],
+        price: "無料",
       }
     };
   },
